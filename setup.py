d050a6b093954401911364067cc36190ad5c6143
Maxime <emixampons@gmail.com>
2021-03-20 15:16:28 +0100

    added back setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyqol",
+    version="0.1.0.0",
+    author="Maxime",
+    author_email="emixampons@gmail.com",
+    description="A Pack of useful python QoL changes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://maxime.codes/Libraries/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "fastcore"
+    ]
+)
